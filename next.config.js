d61aaa0c2c07ce4d3aafaa262ba62a8cac4347e3
piyabhalla <piyabhalla000@gmail.com>
2025-06-28 01:38:10 +0530

    Remove next.config.ts completely

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+ 
 };
 
 module.exports = nextConfig;
