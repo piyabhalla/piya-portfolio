c5a168934dfd11b4e0616a4edffd8fe35a882e26
piyabhalla <piyabhalla000@gmail.com>
2025-06-28 15:41:30 +0530

    Fix: Home page fully mobile responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,21 @@
-// src/app/layout.tsx
 'use client';
 
 import './globals.css';
 import { ReactNode } from 'react';
+import Head from 'next/head';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
         <title>Piya Bhalla Portfolio</title>
-      </head>
-      <body>
-        {children}
-      </body>
+      </Head>
+      <body>{children}</body>
     </html>
   );
 }
